797efd8a69f28d53b830da9595e7d8887f46ba69
laxezice <61070048@kmitl.ac.th>
2020-03-12 03:52:04 +0700

    add admin

--- event/admin.py
+++ event/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from event.models import Event, Category
 # Register your models here.
 
-admin.site.register(Event)
+1
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'event_name', 'event_date', 'event_time', 'ticket_price', 'ticket_amount', 'category']
+admin.site.register(Event, EventAdmin)
 admin.site.register(Category)
